45f316f125e27568dc426b0c7048445697f8639c
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-01-22 09:03:47 +0700

    [kyomel] no 4 task done

--- app3.js
+++ app3.js
@@ -1,30 +1,40 @@
 `use strict`
-
 const fetch = require('node-fetch');
-let api = "https://mul14.github.io/data/employees.json";
+const urlAPI = "https://mul14.github.io/data/employees.json";
 
 async function getEmployee(){
-    let resData = await fetch(`${api}`);
-    let responses = await resData.json();
+    const resData = await fetch(`${urlAPI}`);
+    const responses = await resData.json();
 
-    let salary = 15000000;
+    const salary =15000000;
     let dataSalary = responses.find((item) => {
-        return dataSalary = item.salary === salary ? true : false;
+        return resultSeacrh = item.salary === salary ? true : false;
     });
     console.log('dataSalary', dataSalary);
 
-    let city =  "DKI Jakarta";
-    let dataCity = responses.filter(item) => {
-        return dataCity = item.addresses[0].city === city ? true : false;
+    const city = "DKI Jakarta";
+    let dataCity = responses.filter((item) => {
+        return resultSeacrh = item.addresses[0].city === city ? true : false;
     });
     console.log('dataCity', dataCity);
 
-    let dept = "Research and development";
+    const dept ="Research and development";
     let dataDept = responses.filter((item) => {
-        return dataDept =  item.department.name === dept ?  true : false;
+        return resultSeacrh = item.department.name === dept ?  true : false;
+    });
+    console.log('dataDept', dataDept);
+
+    const birthday = " -04- "
+    let dataBirthday = responses.filter((item) => {
+        return resultSeacrh = item.birthday === birthday ? true : false;
     });
 
-    console.log('dataDept', dataDept);
+    const startDate = "2019-10-01";
+    const EndDate = "2019-10-31";
+    let absense = responses.filter((item) => {
+        return resultSeacrh = item.presence_list[0] > startDate && item.presence_list < EndDate ? true : false;
+    });
+
 }
 
 getEmployee()

e5e9e234fbefdb29d488a58d017732ea0b189801
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-03 00:12:46 +0700

    [kyomel] update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 module.exports = [
     function(req, res, next) {
         res.status(404);
-        next(new Error("Not Found"))
+        next(new Error("Resource Not Found"))
     },
     function (err, req, res, next) {
         console.log(err)

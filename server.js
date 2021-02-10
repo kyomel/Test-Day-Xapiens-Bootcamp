49de3b1395bd3678f77bde74adbec59d04d4fddd
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-10 12:32:24 +0700

    [kyomel] update

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const app = express();
 const log_morgan = require('./src/middlewares/morganlog');
 const errorHandler = require('./src/middlewares/errorHandler');
 
-
-const PORT = 3000;
 const routers = require('./src/routers');
 const { router: routerBull } = require('bull-board');
 
@@ -36,6 +34,4 @@ app.use('/api/v1', routers);
 app.use(Sentry.Handlers.errorHandler());
 errorHandler.forEach(handler => app.use(handler));
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+module.exports = app;

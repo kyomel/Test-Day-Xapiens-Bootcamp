b47829314eac5221abc07d0000b1e083b97770b5
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-10 11:50:41 +0700

    [kyomel] update to deploy

--- config/database.js
+++ config/database.js
@@ -6,17 +6,17 @@ module.exports =
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: `${process.env.DB_NAME}_develop`,
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "localhost",
     dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT
   },
-  // "test": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_test",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME + '_test',
+    host: process.env.DB_HOST || "localhost",
+    dialect: process.env.DB_DIALECT
+  },
   production: {
     dialect: "postgres",
     use_env_variable: "DATABASE_URL"

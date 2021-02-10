7d45a51ddf4f0d963e87c8d58dbfca7f75f49b14
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-11 04:44:01 +0700

    [kyomel] ok

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,4 +11,16 @@ routers.use('/publishers', publisher);
 routers.use('/checkongkir', checkongkir);
 routers.use('/users', user);
 
+routers.use('/', (req, res) => {
+    res.json({
+        "success": true,
+        "message": "welcome to ...!",
+        "data": {
+            "documentation": "https://documenter.getpostman.com/view/10995686/TW77h41n",
+            "login": "https://xapiens-bootcamp.herokuapp.com/api/v1/login",
+            "register": "http://localhost:3000/api/v1/users"
+        }
+    })
+})
+
 module.exports = routers;

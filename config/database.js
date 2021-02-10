a24384fb4984b5b0644a7ffc74a8eb0db735ee0b
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-10 14:37:22 +0700

    [kyomel] update

--- config/database.js
+++ config/database.js
@@ -18,7 +18,11 @@ module.exports =
     dialect: process.env.DB_DIALECT
   },
   production: {
-    dialect: "postgres",
-    use_env_variable: "DATABASE_URL"
+    username: process.env.DB_USER_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: `${process.env.DB_NAME}_prod`,
+    host: process.env.DB_HOST_PROD || "localhost",
+    dialect: process.env.DB_DIALECT_PROD,
+    port: process.env.DB_PORT_PROD
   }
 }

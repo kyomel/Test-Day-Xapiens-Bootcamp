efe0f1b3dcf3d16280c61dda615c5e3d7c8da8fd
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-10 15:38:14 +0700

    [kyomel] done

--- config/database.js
+++ config/database.js
@@ -20,9 +20,15 @@ module.exports =
   production: {
     username: process.env.DB_USER_PROD,
     password: process.env.DB_PASSWORD_PROD,
-    database: `${process.env.DB_NAME}_prod`,
+    database: process.env.DB_NAME_PROD,
     host: process.env.DB_HOST_PROD,
     dialect: process.env.DB_DIALECT_PROD,
-    port: process.env.DB_PORT_PROD
+    port: process.env.DB_PORT_PROD,
+    dialectOptions: {
+      ssl:  {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }

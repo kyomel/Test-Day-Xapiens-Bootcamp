0f4dc8aa9927931770b93fd2933c4613de1c753a
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-10 11:19:21 +0700

    [kyomel] update

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports =
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT
-  }
+  },
   // "test": {
   //   "username": "root",
   //   "password": null,
@@ -17,11 +17,8 @@ module.exports =
   //   "host": "127.0.0.1",
   //   "dialect": "mysql"
   // },
-  // "production": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "database_production",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql"
-  // }
+  production: {
+    dialect: "postgres",
+    use_env_variable: "DATABASE_URL"
+  }
 }

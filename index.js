a8bc62d3b758bc59e2d620eb209a399b5131a0bf
Michael Stevan Lapandio <michael.lapandio@xapiens.id>
2021-02-17 22:11:20 +0700

    [kyomel] add dockerfile

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 const router = require('./router');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 })
